e0438b15053d3e57bb68edfb8dc3698ddcde80b2
Andrei Butenka <andreiButenka@gmail.com>
2019-08-13 23:49:14 +0300

    init: start chat task

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+// import logo from './logo.svg'
+import Chat from '../src/components/Chat/Chat'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Chat />
+      </div>
+    )
+  }
+}
+
+export default App
